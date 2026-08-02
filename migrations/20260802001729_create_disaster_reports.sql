145ea013c53ef7e10f47778cb57e57c200b0f3a9
lalatina11 <rahmadancandra@gmail.com>
2026-08-02 07:21:22 +0700

    refactor: rename table

--- migrations/20260802001729_create_disaster_reports.sql
+++ migrations/20260802001729_create_disaster_reports.sql
@@ -1,14 +1,17 @@
 -- Add migration script here
-CREATE TABLE flood_reports
+CREATE TABLE disaster_reports
 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 
-    CONSTRAINT fk_flood_reports_user
+    CONSTRAINT fk_disaster_reports_user
         FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-)
+);
+
+CREATE INDEX idx_disaster_reports_id ON disaster_reports(id);
+CREATE INDEX idx_disaster_reports_user_id ON disaster_reports(user_id);
