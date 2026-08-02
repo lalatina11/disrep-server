834ba298551fd18cbbdc3701276fa794a1cb8154
lalatina11 <rahmadancandra@gmail.com>
2026-08-02 07:09:20 +0700

    feat: add flood_reports table and update user schema to use UUID type with sqlx support

--- migrations/20260801091513_create_users.sql
+++ migrations/20260801091513_create_users.sql
@@ -1,6 +1,7 @@
 -- Add migration script here
-CREATE TABLE users(
-    id VARCHAR(36) PRIMARY KEY,
+CREATE TABLE users
+(
+    id UUID PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     display_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
