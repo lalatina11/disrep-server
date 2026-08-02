2afb865041c5e41405cfd789fa2b6c2c30e20dd1
lalatina11 <rahmadancandra@gmail.com>
2026-08-02 07:37:12 +0700

    feat: add disaster_reports table schema and initialize storage directory structure

--- migrations/20260802001729_create_disaster_reports.sql
+++ migrations/20260802001729_create_disaster_reports.sql
@@ -3,14 +3,20 @@ CREATE TABLE disaster_reports
 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    street TEXT,
+    city VARCHAR(100) NOT NULL,
+    lat DECIMAL NOT NULL,
+    lng DECIMAL NOT NULL,
+    image DECIMAL NOT NULL,
+    image_storage_url DECIMAL NOT NULL,
+    status TEXT NOT NULL DEFAULT 'pending' CHECK(status IN ('pending', 'new', 'aid_dispatched', 'aid_arrived', 'resolved')) ,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 
-    CONSTRAINT fk_disaster_reports_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    CONSTRAINT fk_disaster_reports_user FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_disaster_reports_id ON disaster_reports(id);
