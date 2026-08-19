be775d9ece421bea69c6c65caf9af3f85e408ba5
lalatina11 <rahmadancandra@gmail.com>
2026-08-19 19:22:57 +0700

    feat: rename DisasterReportImageModel to DisasterAidModel and add attachment payload structures

--- src/models/disaster_aid_model.rs
+++ src/models/disaster_aid_model.rs
@@ -6,7 +6,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Queryable, Selectable, Serialize, Deserialize)]
 #[diesel(table_name = crate::schema::disaster_report_aids)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct DisasterReportImageModel {
+pub struct DisasterAidModel {
     pub id: Uuid,
     pub disaster_report_id: Uuid,
+}
+
+pub struct DisasterAidAttachmentPayload {
+    pub url: String,
+}
+
+pub struct CreateDisasterAid {
+    pub disaster_id: Uuid,
+    pub attachments: Vec<DisasterAidAttachmentPayload>,
 }
