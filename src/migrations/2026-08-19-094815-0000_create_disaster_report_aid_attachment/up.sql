013d464bf9e0f1e281e6c1fe8abb6ad591e86c51
lalatina11 <rahmadancandra@gmail.com>
2026-08-19 18:33:16 +0700

    feat: define DisasterAidAttachmentModel and finalize corresponding database migration schema

--- src/migrations/2026-08-19-094815-0000_create_disaster_report_aid_attachment/up.sql
+++ src/migrations/2026-08-19-094815-0000_create_disaster_report_aid_attachment/up.sql
@@ -5,7 +5,6 @@ CREATE TABLE disaster_report_aid_attachments
     disaster_report_aid_id UUID NOT NULL,
     image_url TEXT NOT NULL,
 
-
     CONSTRAINT fk_disaster_report_aid FOREIGN KEY (disaster_report_aid_id)
     REFERENCES disaster_report_aids(id) ON DELETE CASCADE ON UPDATE CASCADE
 
--- src/models/disaster_aid_attachment_model.rs
+++ src/models/disaster_aid_attachment_model.rs
@@ -1,1 +1,13 @@
-pub struct DisasterAidImageModel {}
+use serde::{Deserialize, Serialize};
+
+use diesel::prelude::*;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable, Selectable)]
+#[diesel(table_name=crate::schema::disaster_report_aid_attachments)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct DisasterAidAttachmentModel {
+    id: Uuid,
+    disaster_report_aid_id: Uuid,
+    image_url: String,
+}
