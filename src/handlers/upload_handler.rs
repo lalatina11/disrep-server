ad1d8f57204d4b68a35b399f57aa4ebfe1aa145e
lalatina11 <rahmadancandra@gmail.com>
2026-08-20 09:49:49 +0700

    refactor: transition upload handlers to use ParsedSupabaseResult

--- src/handlers/upload_handler.rs
+++ src/handlers/upload_handler.rs
@@ -3,25 +3,29 @@ use axum::extract::Multipart;
 use crate::{
     service::upload_service::UploadService,
     utils::responses::{
-        api_responses::ApiResponseReturnTypeWithHeader, storage_response::StorageResultURL,
+        api_responses::ApiResponseReturnTypeWithHeader, storage_response::ParsedSupabaseResult,
     },
 };
 
 pub struct UploadHandler;
 
 impl UploadHandler {
-    pub async fn image(multipart: Multipart) -> ApiResponseReturnTypeWithHeader<StorageResultURL> {
+    pub async fn image(
+        multipart: Multipart,
+    ) -> ApiResponseReturnTypeWithHeader<ParsedSupabaseResult> {
         let service = UploadService::upload_image(multipart).await;
         match service {
             Err(err) => err.to_handler_error(),
-            Ok(res) => res.into_response(),
+            Ok(res) => res.parsed().into_response(),
         }
     }
-    pub async fn video(multipart: Multipart) -> ApiResponseReturnTypeWithHeader<StorageResultURL> {
+    pub async fn video(
+        multipart: Multipart,
+    ) -> ApiResponseReturnTypeWithHeader<ParsedSupabaseResult> {
         let service = UploadService::upload_video(multipart).await;
         match service {
             Err(err) => err.to_handler_error(),
-            Ok(res) => res.into_response(),
+            Ok(res) => res.parsed().into_response(),
         }
     }
 }
--- src/utils/responses/storage_response.rs
+++ src/utils/responses/storage_response.rs
@@ -16,10 +16,25 @@ pub struct SupabaseStorageResult {
     pub id: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ParsedSupabaseResult {
+    pub media_url: String,
+    pub media_id: String,
+}
+
 impl SupabaseStorageResult {
-    pub fn into_response(self) -> ApiResponseReturnTypeWithHeader<StorageResultURL> {
+    pub fn parsed(self) -> ParsedSupabaseResult {
+        ParsedSupabaseResult {
+            media_url: self.key,
+            media_id: self.id,
+        }
+    }
+}
+
+impl ParsedSupabaseResult {
+    pub fn into_response(self) -> ApiResponseReturnTypeWithHeader<ParsedSupabaseResult> {
         ApiResponse::success(
-            Some(StorageResultURL { url: self.key }),
+            Some(self),
             Some("Success to upload file!".to_string()),
             Some(StatusCode::CREATED),
         )
