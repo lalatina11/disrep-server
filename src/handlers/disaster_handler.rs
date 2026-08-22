2ccf03354595eea1a732b7ac1138310bffe1f532
lalatina11 <rahmadancandra@gmail.com>
2026-08-22 20:14:15 +0700

    fix: correct typo in disaster handler admin authorization method name

--- src/handlers/disaster_handler.rs
+++ src/handlers/disaster_handler.rs
@@ -52,7 +52,7 @@ impl DisasterHandler {
                     return ApiResponse::success(Some(data), None, None);
                 }
                 if let Some(user) = authenticated {
-                    if data.disaster.status == "pending" && user.is_authorize_as_admin() {
+                    if data.disaster.status == "pending" && user.is_authorize_as_admins() {
                         return ApiResponse::success(Some(data), None, None);
                     }
                 }
