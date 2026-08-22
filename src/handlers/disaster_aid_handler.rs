ac70a097d67ca2b4beabf0e192aedeb566422bc7
lalatina11 <rahmadancandra@gmail.com>
2026-08-22 20:17:14 +0700

    refactor: remove unused user model extension and debug print from disaster aid creation handler

--- src/handlers/disaster_aid_handler.rs
+++ src/handlers/disaster_aid_handler.rs
@@ -1,7 +1,5 @@
-use axum::Extension;
-
 use crate::{
-    models::{disaster_aid_model::CreateDisasterAid, user_model::UserModel},
+    models::disaster_aid_model::CreateDisasterAid,
     service::disaster_report_aid_service::DiassterReportAidService,
     utils::{
         request::json_parser::JsonParser,
@@ -13,10 +11,8 @@ pub struct DisasterAidHandler;
 
 impl DisasterAidHandler {
     pub async fn create(
-        Extension(_user): Extension<UserModel>,
         JsonParser(payload): JsonParser<CreateDisasterAid>,
     ) -> ApiResponseReturnTypeWithHeader<bool> {
-        println!("{:?}", _user);
         let service = DiassterReportAidService::create(payload).await;
         match service {
             Err(err) => err.to_handler_error(),
