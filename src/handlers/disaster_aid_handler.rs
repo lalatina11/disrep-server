db339306fe4f5566589c51c67b1c185733b3482e
lalatina11 <rahmadancandra@gmail.com>
2026-08-22 20:34:13 +0700

    feat: update disaster aid handler to return DisasterWithAllRelations instead of boolean

--- src/handlers/disaster_aid_handler.rs
+++ src/handlers/disaster_aid_handler.rs
@@ -1,6 +1,9 @@
 use crate::{
     models::disaster_aid_model::CreateDisasterAid,
-    service::disaster_report_aid_service::DiassterReportAidService,
+    service::{
+        disaster_report_aid_service::DiassterReportAidService,
+        disaster_service::DisasterWithAllRelations,
+    },
     utils::{
         request::json_parser::JsonParser,
         responses::api_responses::{ApiResponse, ApiResponseReturnTypeWithHeader},
@@ -12,11 +15,11 @@ pub struct DisasterAidHandler;
 impl DisasterAidHandler {
     pub async fn create(
         JsonParser(payload): JsonParser<CreateDisasterAid>,
-    ) -> ApiResponseReturnTypeWithHeader<bool> {
+    ) -> ApiResponseReturnTypeWithHeader<DisasterWithAllRelations> {
         let service = DiassterReportAidService::create(payload).await;
         match service {
             Err(err) => err.to_handler_error(),
-            Ok(_) => ApiResponse::success(None, None, None),
+            Ok(data) => ApiResponse::success(Some(data), None, None),
         }
     }
 }
