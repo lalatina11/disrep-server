e96fab70f1982eee08cf278d2526297d924ac1f5
lalatina11 <rahmadancandra@gmail.com>
2026-09-01 10:39:41 +0700

    feat: add media_preview for previewing image

--- src/utils/responses/storage_response.rs
+++ src/utils/responses/storage_response.rs
@@ -1,7 +1,10 @@
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 
-use crate::utils::responses::api_responses::{ApiResponse, ApiResponseReturnTypeWithHeader};
+use crate::utils::{
+    CommonUtility,
+    responses::api_responses::{ApiResponse, ApiResponseReturnTypeWithHeader},
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct StorageResultURL {
@@ -21,14 +24,17 @@ pub struct ParsedSupabaseResult {
     pub media_url: String,
     pub media_id: String,
     pub media_type: String,
+    pub media_preview: String,
 }
 
 impl SupabaseStorageResult {
     pub fn parsed(self, media_type: String) -> ParsedSupabaseResult {
+        let media_preview = CommonUtility::generate_media_url(self.key.clone());
         ParsedSupabaseResult {
             media_url: self.key,
             media_id: self.id,
             media_type,
+            media_preview,
         }
     }
 }
