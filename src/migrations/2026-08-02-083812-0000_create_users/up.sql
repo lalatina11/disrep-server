eade4d07fbe64bc017c51ff5eca9507779c68dac
lalatina11 <rahmadancandra@gmail.com>
2026-09-03 17:40:35 +0700

    refactor: normalize SQL schema definitions for users and disaster reports tables

--- src/migrations/2026-08-02-083812-0000_create_users/up.sql
+++ src/migrations/2026-08-02-083812-0000_create_users/up.sql
@@ -6,7 +6,6 @@ CREATE TABLE users
     display_name VARCHAR(255) NOT NULL,
     role TEXT NOT NULL DEFAULT 'user',
     avatar TEXT,
-    avatar_storage_url TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
--- src/migrations/2026-08-02-083955-0000_create_disaster_reports/up.sql
+++ src/migrations/2026-08-02-083955-0000_create_disaster_reports/up.sql
@@ -1,22 +1,22 @@
 -- Your SQL goes here
-CREATE TABLE disaster_reports
-(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT,
-    street TEXT,
-    city VARCHAR(100) NOT NULL,
-    lat DOUBLE PRECISION NOT NULL,
-    lng DOUBLE PRECISION NOT NULL,
-    is_anon BOOLEAN DEFAULT false,
-    status TEXT NOT NULL DEFAULT 'pending' CHECK(status IN ('pending', 'new', 'aid_dispatched', 'aid_arrived', 'resolved')) ,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-
-    CONSTRAINT fk_disaster_reports_user FOREIGN KEY (user_id)
-    REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE disaster_reports (
+    id          UUID          DEFAULT gen_random_uuid() PRIMARY KEY,
+    user_id     UUID          NOT NULL,
+    title       TEXT          NOT NULL,
+    description TEXT         ,
+    street      TEXT         ,
+    city        VARCHAR (100) NOT NULL,
+    lat         FLOAT         NOT NULL,
+    lng         FLOAT         NOT NULL,
+    is_anon     BOOLEAN       DEFAULT false NOT NULL,
+    status      TEXT          DEFAULT 'pending' NOT NULL,
+    created_at  TIMESTAMPTZ   DEFAULT NOW() NOT NULL,
+    updated_at  TIMESTAMPTZ   DEFAULT now() NOT NULL,
+    CONSTRAINT fk_disaster_reports_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE INDEX idx_disaster_reports_id ON disaster_reports(id);
-CREATE INDEX idx_disaster_reports_user_id ON disaster_reports(user_id);
+CREATE INDEX idx_disaster_reports_id
+    ON disaster_reports(id);
+
+CREATE INDEX idx_disaster_reports_user_id
+    ON disaster_reports(user_id);
