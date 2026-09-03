6d1224836a0e1a6f6bbabfab38e3e97f159b064d
lalatina11 <rahmadancandra@gmail.com>
2026-09-03 14:26:18 +0700

    refactor: clean up body size

--- src/routes/upload_routes.rs
+++ src/routes/upload_routes.rs
@@ -2,6 +2,8 @@ use axum::{Router, extract::DefaultBodyLimit, routing::post};
 
 use crate::handlers::upload_handler::UploadHandler;
 
+const ONE_MEGA_BYTES: usize = 1024 * 1024;
+
 pub struct UploadRoutes;
 
 impl UploadRoutes {
@@ -9,6 +11,6 @@ impl UploadRoutes {
         Router::new()
             .route("/image", post(UploadHandler::image))
             .route("/video", post(UploadHandler::video))
-            .layer(DefaultBodyLimit::max(10 * 1024 * 1024))
+            .layer(DefaultBodyLimit::max(500 * ONE_MEGA_BYTES)) // 500 MB
     }
 }
