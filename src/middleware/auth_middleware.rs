db5fd0eddcb55af81fdaf0acb039c59e4e9d0366
lalatina11 <rahmadancandra@gmail.com>
2026-09-07 11:10:06 +0700

    refactor: clean up get access token from headermap

--- src/middleware/auth_middleware.rs
+++ src/middleware/auth_middleware.rs
@@ -1,13 +1,17 @@
 use axum::{extract::Request, middleware::Next, response::IntoResponse};
 use reqwest::StatusCode;
 
-use crate::{service::auth_service::AuthService, utils::responses::api_responses::ApiResponse};
+use crate::{
+    service::auth_service::AuthService,
+    utils::{CommonUtility, responses::api_responses::ApiResponse},
+};
 
 pub struct AuthMiddleware;
 
 impl AuthMiddleware {
     pub async fn handle(mut req: Request, next: Next) -> impl IntoResponse {
-        let user_payload = AuthService::get_user(req.headers()).await;
+        let token = CommonUtility::get_access_token_from_headers(req.headers());
+        let user_payload = AuthService::get_user(token).await;
 
         match user_payload {
             Err(_) => {
@@ -22,7 +26,8 @@ impl AuthMiddleware {
     }
 
     pub async fn optional(mut req: Request, next: Next) -> impl IntoResponse {
-        let user_payload = AuthService::get_user(req.headers()).await;
+        let token = CommonUtility::get_access_token_from_headers(req.headers());
+        let user_payload = AuthService::get_user(token).await;
         if let Ok(data) = user_payload {
             req.extensions_mut().insert(data.fix_avatar_url());
         }
